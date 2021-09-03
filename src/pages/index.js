6fee449f07b032279ecc215f4bbfcc3bf8f77ced
gustavo3x2 <63024235+gustavo3x2@users.noreply.github.com>
2021-09-03 18:47:28 -0300

    VideoCard

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,76 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Box, Grid } from '@material-ui/core';
 
-import Layout from '../componnents/Layout';
+import Layout from 'src/componnents/Layout';
+import VideoCard from 'src/componnents/VideoCard';
 
-export default function Home() {
+function Home({ data }) {
   return (
     <Layout title="YouTube">
-      Clone youtube com Next js + Material-UI
-      <Button variant="outlined" color="secondary">Primary</Button>
+      <Box p={2}>
+        <Grid container spacing={4}>
+          {data.map((item) => (
+            <Grid key={item._id} item xl={3} lg={3} md={4} sm={6} xs={12}>
+              <VideoCard item={item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Layout>
-
   );
 }
+
+export async function getStaticProps() {
+  const data = [
+    {
+      id: 1,
+      title: 'NEXT.JS: O FRAMEWORK QUE VOCÊ DEVERIA CONHECER [PARTE #01]',
+      authorId: 1,
+      authorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next01.png',
+      videoUrl: 'url',
+      updateAt: new Date(),
+    },{
+      id: 2,
+      title: 'NEXT.JS: ENTENDENDO A RENDERIZAÇÃO REACT NO LADO SERVIDOR [PARTE #02]',
+      authorId: 1,
+      authorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next02.png',
+      videoUrl: 'url',
+      updateAt: new Date(),
+    },{
+      id: 3,
+      title: 'NEXT.JS: ROTAS ESTÁTICAS E DINAMICAS [PARTE #03]',
+      authorId: 1,
+      authorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next03.png',
+      videoUrl: 'url',
+      updateAt: new Date(),
+    },{
+      id: 4,
+      title: 'NEXT.JS: ROTAS ESTÁTICAS E DINAMICAS [PARTE #03]',
+      authorId: 1,
+      authorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next01.png',
+      videoUrl: 'url',
+      updateAt: new Date(),
+    },
+  ];
+
+  return {
+    props: {
+      data: JSON.parse(JSON.stringify(data)),
+    }, // will be passed to the page component as props
+    revalidate: 15,
+  };
+}
+
+export default Home;
