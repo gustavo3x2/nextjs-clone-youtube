8b4aaf547da294269a309931f2c9b2d9179d35bb
gustavo3x2 <63024235+gustavo3x2@users.noreply.github.com>
2021-09-03 15:24:01 -0300

    Voltando

--- src/theme/index.js
+++ src/theme/index.js
@@ -11,7 +11,7 @@ const theme = createTheme({
       paper: colors.common.white,
     },
     primary: {
-      main: '#f44336',
+      main: '#green',
     },
     secondary: {
       main: '#3EA6FF',
